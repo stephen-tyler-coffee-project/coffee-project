db37a7a06317ba5e90c8b43f1cd2018fbc0555a3
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-01-11 09:19:17 -0600

    updated coffee project

--- main.js
+++ main.js
@@ -80,23 +80,23 @@ divBody.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
 
-function sort_by_id(){
-    return function (elem1, elem2){
-        if (elem1.id <elem2.id) {
-            return -1;
-        } else if (elem1.id > elem2.id) {
-            return 1;
-        } else {
-            return 0;
-        }
-    };
-}
-console.log(coffees.sort(sort_by_id()));
-
-var sortByID = coffees.sort((a,b) => {
-    return a.id - b.id;
-});
-console.log(sortByID);
+// function sort_by_id(){
+//     return function (elem1, elem2){
+//         if (elem1.id <elem2.id) {
+//             return -1;
+//         } else if (elem1.id > elem2.id) {
+//             return 1;
+//         } else {
+//             return 0;
+//         }
+//     };
+// }
+// console.log(coffees.sort(sort_by_id()));
+//
+// var sortByID = coffees.sort((a,b) => {
+//     return a.id - b.id;
+// });
+// console.log(sortByID);
 
 
 
